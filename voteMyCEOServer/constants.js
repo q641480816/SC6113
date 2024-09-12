765b38079064019266eaa8e4638c6e980424db6d
JL <q641480816@gmail.com>
2024-09-12 21:54:47 +0800

    initial commit for server

---
+++ voteMyCEOServer/constants.js
@@ -0,0 +1,10 @@
+const Constants = {
+    threshold: 4000,
+    voteContractAddress: "0x408A59ca2c8b87B90b22D25DCbd9056173079EF2",
+    ownerPrivate: "0x2c817c7079125936d990087e42b49a8d0a954408c0b4b13a0f66fe9977501774",
+    // ownerPrivate: "0xfbbb013f9c578f637f99a139877fc530b8d3a03257b729b0b2694567c0507132",
+    hashAlgo: 'sha256',
+    uploader: ['0x2c817c7079125936d990087e42b49a8d0a954408c0b4b13a0f66fe9977501774', '0x14cffd60b76a32d4e7324747af595f709b7cbe500be3367432e6f920d4790b36']
+};
+
+module.exports = { Constants };
