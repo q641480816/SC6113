3295f5e3f1fcf8e6c885e948883b9254a953c98b
JL <q641480816@gmail.com>
2024-09-12 21:14:52 +0800

    initail commit for indexer

---
+++ indexer/contractService.js
@@ -0,0 +1,104 @@
+const { ethers } = require("ethers");
+const votingContractABI = require('./abi/VotingCampaignContract.json').abi;
+const tokenContractABI = require('./abi/VotingToken.json').abi;
+const { hsetAdd, hgetAll, hsetGet } = require('./redisClient');
+const properties = require('./properties.json');
+const WebSocket = require('ws');
+
+let wallet, wsProvider, contract, jsonProvider, tokenContract;
+
+const getJsonProvider = () => {
+    if (jsonProvider) return jsonProvider;
+    jsonProvider = new ethers.JsonRpcProvider(`http${properties.ssl}://${properties.rpcURL}`);
+    return jsonProvider;
+}
+
+const getWebSocket = () => {
+    const ws = new WebSocket(`ws${properties.ssl}://${properties.rpcURL}`);
+
+    ws.on("close", () => {
+        console.log("Disconnected. Reconnecting...");
+        setTimeout(() => {
+            reconnect();
+        }, 1000);
+    });
+
+    ws.on("error", (error) => {
+        console.log("WebSocket error: ", error);
+    });
+
+    return ws;
+}
+
+const startListening = () => {
+    console.log('Initating connection to RPC...')
+    wsProvider = new ethers.WebSocketProvider(getWebSocket());
+    contract = new ethers.Contract(properties.votingContractAddress, votingContractABI, wsProvider);
+    jsonProvider = new ethers.JsonRpcProvider(`http${properties.ssl}://${properties.rpcURL}`)
+    tokenContract = new ethers.Contract(properties.tokenContractAddress, tokenContractABI, wsProvider);
+
+    // Listen for events
+    contract.on("UploaderChange", (address, isAllowed) => {
+        console.log(`${address} is set to${isAllowed}`);
+    });
+
+    contract.on("VoteCampaignChange", (campaignId, campaignName, metadata, options, results, isActive) => {
+        try {
+            const campaignString = JSON.stringify({
+                campaignId: Number(campaignId),
+                campaignName: campaignName,
+                metadata: metadata,
+                options: options,
+                results: results.map(v => Number(v)),
+                isActive: isActive
+            })
+            hsetAdd(properties.tables.campaignMap, campaignId, campaignString);
+            console.log('New campaign: ' + campaignId)
+        } catch (e) {
+            console.log(e)
+        }
+    });
+
+    tokenContract.on("FaucetToAddress", (address, timestamp, amount) => {
+        try {
+            let list = [];
+            hgetAll(properties.tables.faucetHistory)
+                .then(allTenHis => {
+                    if (Object.keys(allTenHis).length > 0) {
+                        JSON.parse(allTenHis.list)
+                            .forEach(h => list.push(h));
+                    }
+                    list.push({
+                        address: address,
+                        timestamp: timestamp + '',
+                        amount: ethers.formatUnits(amount),
+                    })
+                    list = list.sort((a, b) => Number(b.timestamp) - Number(a.timestamp));
+                    if (list.length > 10) list = list.slice(0, 10);
+                    hsetAdd(properties.tables.faucetHistory, 'list', JSON.stringify(list));
+                })
+                .catch(err => { throw err });
+        } catch (e) {
+            console.log(e)
+        }
+    })
+
+    wsProvider.on('error', (err) => {
+        console.error('WebSocket error:', err);
+        // Try to reconnect on error
+        reconnect();
+    });
+}
+
+const reconnect = () => {
+    if (contract) { contract.removeAllListeners(); }
+    if (tokenContract) { contract.removeAllListeners(); }
+    if (wsProvider) { wsProvider.destroy(); }
+
+    console.log('Reconnecting in 1 seconds...');
+    // setTimeout(startListening, 1000);
+    startListening();
+
+}
+
+module.exports = { startListening, reconnect, faucet, syncCamping }
---
+++ indexer/indexer.js
@@ -0,0 +1,66 @@
+const os = require('os');
+const cluster = require('cluster');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const { flush, hsetAdd, hsetGet, hgetAll } = require('./redisClient');
+const properties = require('./properties.json');
+// const numCPUs = Math.ceil(os.cpus().length / 2);
+const numCPUs = 2;
+const { startListening, faucet, syncCamping } = require('./contractService');
+const path = require('path');
+const corsOptions = {
+    origin: [`http://localhost:8722`, properties.serverUrl],
+    optionsSuccessStatus: 200
+}
+
+const test = async (contract, id) => {
+    console.log(await contract.getCampaign(id));
+}
+
+if (cluster.isPrimary) {
+    for (let i = 0; i < numCPUs; i++) {
+        const worker = cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        cluster.fork();
+    });
+
+    startListening();
+
+} else {
+    const app = express();
+    const apiRouter = express.Router();
+    const staticFilesPath = path.join(__dirname, 'public');
+
+    // Serve static files using the apiRouter
+    app.use('/indexer', apiRouter);
+    app.use(cors(corsOptions));
+    const jsonParser = bodyParser.json();
+
+    apiRouter.get('/getFaucetHistory', async (req, res) => {
+        const faucetHistoryList = await hgetAll(properties.tables.faucetHistory);
+        if(faucetHistoryList.list){
+            res.send(JSON.parse(faucetHistoryList.list));
+        }else{
+            res.send([]);
+        }
+    });
+
+    apiRouter.get('/getCampaign', async (req, res) => {
+        const campaign = await hgetAll(properties.tables.campaignMap);
+        
+        res.send(Object.values(campaign).map(c => {
+            const campaign = JSON.parse(c);
+            console.log(campaign)
+            if(!isNaN(c)) return c;
+            campaign.metadata = JSON.parse(campaign.metadata);
+            return campaign;
+        }).filter(c => c.campaignId !== undefined && c.campaignId !== null));
+    })
+
+    app.listen(properties.port, () => {
+        console.log(`Indexer started at: ${properties.port}`)
+    });
+}
