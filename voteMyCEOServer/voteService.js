18a42c2f2424f6b76011db16f2cf794da76bd451
LILIXZ <xuanzilee1996@gmail.com>
2024-09-12 22:04:10 +0800

    update server

--- voteMyCEOServer/voteService.js
+++ voteMyCEOServer/voteService.js
@@ -4,10 +4,12 @@ const cluster = require('cluster');
 const { getProvider, setUploader, sendTransaction, getContract, verifySignedMessage } = require('./web3Utils');
 const properties = require('./properties.json');
 
+
 const sampleCampaign = {
     name: 'Sample Campaign',
     options: ['A', 'B', 'C', 'D']
 }
+
 
 const validateContract = async () => {
     try {
@@ -19,6 +21,7 @@ const validateContract = async () => {
     }
 }
 
+
 const setUploaderWithAddress = async (addr) => {
     const logger = require('./logger');
     try {
@@ -29,36 +32,65 @@ const setUploaderWithAddress = async (addr) => {
     }
 }
 
+
 const getCampaignResults = async (id) => {
     let contract = getContract();
     const campaign = await contract.methods.getCampaignResults(id).call();
     return campaign;
 }
 
+
+const getCampaign = async (id) => {
+    let contract = getContract();
+    const campaign = await contract.methods.getCampaign(id).call();
+    return {
+        campaignId: id,
+        campaignName: campaign[1],
+        metadate: campaign[2],
+        options: campaign[3],
+        results: campaign[4],
+        isActive: campaign[5]
+    };
+}
+
+
 const startCampaign = async (name, options) => {
     const logger = require('./logger');
     const campaignTable = `campaignList`;
     const contract = getContract();
     try {
-    // assume we have the id now
-    
-        const id = 1;
-        const hkey = `${id}pending`;
-        const skey = `${id}pendingVoted`;
-
-        await redis.hsetAdd(hkey, 'temp', 'temp');
-        await redis.hsetDelete(hkey, 'temp');
-        await redis.setAdd(skey, 'temp');
-        await redis.setRemove(skey, 'temp');
-        logger.info(`Campaign ${name} has been updated to redis`);
-        return id;
+        const receipt = await sendTransaction("beginVoteCampaign", [name, options]);
+        const events = await contract.getPastEvents('BeginVoteCampaign', {
+            filter: {},
+            fromBlock: receipt.blockNumber,
+            toBlock: receipt.blockNumber
+        });
+        const campaignId = parseInt(events[0]['returnValues'][2]);
+        await redis.hsetAdd(campaignTable, campaignId, JSON.stringify({ name: name, options: options, 3: true }));
+        logger.info(`Campain ${name}: ${JSON.stringify(options)} has been started and broadcasted on chain.`);
+        return "Started";
     } catch (err) {
         throw err;
     }
 
 
+    //assume we have the id now
+    //
+    //     const id = 1;
+    //     const hkey = `${id}pending`;
+    //     const skey = `${id}pendingVoted`;
+
+
+    //     await redis.hsetAdd(hkey, 'temp', 'temp');
+    //     await redis.hsetDelete(hkey, 'temp');
+    //     await redis.setAdd(skey, 'temp');
+    //     await redis.setRemove(skey, 'temp');
+    //     logger.info(`Campaign ${name} has been updated to redis`);
+    //     return id;
+
 
 }
+
 
 const endCampaign = async (campaignId) => {
     const logger = require('./logger');
@@ -71,6 +103,7 @@ const endCampaign = async (campaignId) => {
     }
 }
 
+
 const castVote = async (address, message, campaignId, signature = null) => {
     const logger = require('./logger');
     //TODO: verify message with wallet public key and retrieve message
@@ -78,14 +111,17 @@ const castVote = async (address, message, campaignId, signature = null) => {
     const skey = `${campaignId}pendingVoted`;
     const campaignTable = properties.tables.campaignMap;
 
+
     // if (signature && !verifySignedMessage(address, message.option, signature)) {
     //     throw new Error("Signature not match");
     // }
+
 
     try {
         //check if user's vote is valid
         const isMember = await redis.sismember(skey, address);
         if (isMember === 1) throw Error(`Address ${address} has already voted for campaignId: ${campaignId}`);
+
 
         //get campaign info
         let campaign = await redis.hsetGet(campaignTable, campaignId);
@@ -98,15 +134,19 @@ const castVote = async (address, message, campaignId, signature = null) => {
         if (!campaign['isActive']) throw Error(`Campaign [${campaignId}] ${campaign['name']} is not active!`);
         if (campaign['options'].indexOf(message.option) < 0) throw Error(`User vote [${message.option}] is not valid Options! Valid options: [${campaign['options'].join(', ')}].`);
 
+
         //add user to pending pool
+        await redis.hsetAdd(hkey, address, message.option);
+        await redis.setAdd(skey, address);
+        // logger.info(`Address ${address} has been added to pending pool of campaignId: ${campaignId} with option: ${message.option}`);
 
-        logger.info(`Address ${address} has been added to pending pool of campaignId: ${campaignId} with option: ${message.option}`);
 
-        const pendingCount = await redis.hsetCount(hkey);
-        if (pendingCount >= Constants.threshold) {
-            logger.info(`CampaignId: ${campaignId} has hit the threshold, start compiling votes...`);
-            compileVote(campaignId, campaign);
-        }
+        // const pendingCount = await redis.hsetCount(hkey);
+        // if (pendingCount >= Constants.threshold) {
+        //     logger.info(`CampaignId: ${campaignId} has hit the threshold, start compiling votes...`);
+        //     compileVote(campaignId, campaign);
+        // }
+
 
         //update master
         process.send({ type: 'request', id: Date.now() + Math.random(), data: { name: 'vote', data: campaignId } });
@@ -118,8 +158,10 @@ const castVote = async (address, message, campaignId, signature = null) => {
     }
 }
 
+
 const checkPendingCount = (campaignId) => {
     const logger = require('./logger');
+
 
     const primary = (campaignId) => {
         return new Promise(async (resolve, reject) => {
@@ -128,10 +170,12 @@ const checkPendingCount = (campaignId) => {
                 const isLocked = await redis.lockTable(hkey);
                 // logger.info(`Lock acquired for ${hkey}, start compiling votes`)
 
+
             } catch (err) {
                 logger.alert(`Error in aquiring lock, aborting this vote compile...`);
                 reject(err);
             }
+
 
             try {
                 const pendingCount = await redis.hsetCount(hkey);
@@ -139,6 +183,7 @@ const checkPendingCount = (campaignId) => {
                     logger.info(`CampaignId: ${campaignId} has hit the threshold, start compiling votes...`);
                     const allAddress = await redis.hsetGetKeys(hkey);
                     const addressToCompile = allAddress.slice(0, properties.countThreshhold);
+
 
                     const pipeline = redis.redisClient.multi();
                     pipeline.hmget(hkey, ...addressToCompile);
@@ -158,6 +203,7 @@ const checkPendingCount = (campaignId) => {
         })
     }
 
+
     return new Promise((resolve, reject) => {
         if (cluster.isPrimary) {
             primary(campaignId)
@@ -176,11 +222,13 @@ const checkPendingCount = (campaignId) => {
                 }
             }
 
+
             process.on('message', requestHandler);
             process.send({ type: 'request', id: uniqueId, data: { name: 'checkPendingCount', data: campaignId } });
         }
     })
 }
+
 
 //implement lock for possible race condition
 const compileVote = async (campaignId) => {
@@ -189,39 +237,70 @@ const compileVote = async (campaignId) => {
     const hCompletedkey = `${campaignId}Completed`;
     const skey = `${campaignId}pendingVoted`;
     let votes, tree, merkleRoot, batchCount, processedVote;
+    const campaign = await getCampaign(campaignId);
 
-    const campaign = sampleCampaign;
-    try {
-        const isLocked = await redis.lockTable(hkey);
 
-        //if no lock, call in 2 sec again and end current process
-        if (!isLocked) {
-            setTimeout(() => compileVote(campaignId, campaign), 2000);
-            return;
-        }
-        logger.info(`Lock acquired for ${hkey}, start compiling votes`)
-    } catch (err) {
-        logger.alert(`Error in aquiring lock, aborting this vote compile...`);
-        return;
-    }
+    //get campaign;
+    // const campaign = sampleCampaign;
+    // try {
+    //     const isLocked = await redis.lockTable(hkey);
+
+
+    //     //if no lock, call in 2 sec again and end current process
+    //     if (!isLocked) {
+    //         setTimeout(() => compileVote(campaignId, campaign), 2000);
+    //         return;
+    //     }
+    //     logger.info(`Lock acquired for ${hkey}, start compiling votes`)
+    // } catch (err) {
+    //     logger.alert(`Error in aquiring lock, aborting this vote compile...`);
+    //     return;
+    // }
+
 
     //proceed safely with lock
     try {
-        // check current count, if not enough end process and release lock
-        const pendingCount = await redis.hsetCount(hkey);
-        if (pendingCount < Constants.threshold) {
-            return;
-        }
+        //check current count, if not enough end process and release lock
+        // const pendingCount = await redis.hsetCount(hkey);
+        // if (pendingCount < Constants.threshold) {
+        //     return;
+        // }
 
-        const allAddress = await redis.hsetGetKeys(hkey);
-        const addressToCompile = allAddress.slice(0, Constants.threshold);
 
-        //prepare redis transaction
-        const pipeline = redis.redisClient.multi();
-        pipeline.hmget(hkey, ...addressToCompile);
-        pipeline.hdel(hkey, ...addressToCompile);
-        const results = await pipeline.exec();
-        votes = addressToCompile.map((adr, i) => [adr, results[0][1][i]]);
+        // const allAddress = await redis.hsetGetKeys(hkey);
+        // const addressToCompile = allAddress.slice(0, Constants.threshold);
+
+
+        // //prepare redis transaction
+        // const pipeline = redis.redisClient.multi();
+        // pipeline.hmget(hkey, ...addressToCompile);
+        // pipeline.hdel(hkey, ...addressToCompile);
+        // const results = await pipeline.exec();
+        // votes = addressToCompile.map((adr, i) => [adr, results[0][1][i]]);
+        votes = await checkPendingCount(campaignId);
+        if (votes.length === 0) return;
+
+
+        const tempCount = campaign['options'].map(option => 0);
+        const hashedVotes = votes.map(v => {
+            tempCount[campaign['options'].indexOf(v[1])]++;
+            return [v[0], cryptoUtils.hash(JSON.stringify({
+                address: v[0],
+                option: v[1],
+                currentCount: [...tempCount]
+            }))];
+        });
+        tree = cryptoUtils.buildMerkleTree(hashedVotes);
+        merkleRoot = tree.root;
+        processedVote = votes.map((v, i) => {
+            return {
+                address: v[0],
+                option: v[1],
+                hash: hashedVotes[i][1],
+                merkleProof: cryptoUtils.getMerkleProof(v[0], tree)
+            }
+        });
+        batchCount = [...tempCount];
     } catch (err) {
         console.log(err);
         logger.error(`Error in compile vote process, aborting this vote compile and releasing lock...`);
@@ -231,6 +310,7 @@ const compileVote = async (campaignId) => {
     //     logger.info(`Lock for ${hkey} has been released.`);
     //     redis.releaseTable(hkey);
     // }
+
 
     //submit to smart contract
     try {
@@ -244,7 +324,9 @@ const compileVote = async (campaignId) => {
         // });
        
 
+
         const batchId = Number(result[1]);
+
 
         //sumarize all counted votes with batchId
         const processedVoteForCompleted = processedVote.reduce((a, n) => {
@@ -252,8 +334,10 @@ const compileVote = async (campaignId) => {
             return a;
         }, {});
 
+
         await redis.hsetMassSet(hCompletedkey, processedVoteForCompleted);
         logger.info(`Batch result [${batchCount.join(", ")}] has been submited to ${campaignId}`);
+
 
     } catch (err) {
         logger.error(`Error in submitting transaction, reinsert votes to pending...`);
@@ -266,8 +350,24 @@ const compileVote = async (campaignId) => {
     }
 }
 
+
+const getUserVoteByCampaign = async (address, campaignId) => {
+    const hkey = `${campaignId}pending`;
+    const hCompletedkey = `${campaignId}Completed`;
+
+
+    const pendingUser = await redis.hsetGet(hkey, address);
+    const completedUser = await redis.hsetGet(hCompletedkey, address);
+
+
+    if (completedUser) return JSON.parse(completedUser);
+    return { option: pendingUser };
+}
+
+
 const verifyMerkleProof = async (address, voteHash, merkleProof, merkleRoot) => {
     return cryptoUtils.verifyMerkleProof(address, voteHash, merkleProof, merkleRoot);
 }
 
-module.exports = { compileVote, validateContract, startCampaign, endCampaign, castVote, setUploaderWithAddress, getCampaignResults, verifyMerkleProof, checkPendingCount };
+
+module.exports = { compileVote, validateContract, startCampaign, endCampaign, castVote, setUploaderWithAddress, getCampaignResults, getUserVoteByCampaign, verifyMerkleProof, checkPendingCount };
